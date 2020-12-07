dbb34d1ddbe1af08ba198096810873577f178e0d
Edgar Lei <edgarlei@users.noreply.github.com>
2020-12-07 08:42:50 -0500

    create

---
+++ source.py
@@ -0,0 +1,18 @@
+# meat of project
+# where all of code goes
+# --- Imports ---------------------------
+import pandas as pd
+import numpy as np
+import scipy as sp
+from scipy import stats
+import math
+import statistics as st
+import matplotlib.pyplot as plt
+
+# Inline Plots
+%matplotlib inline
+
+# For High-Res Plots
+%config InlineBackend.figure_formats = ['svg']
+
+# --- Main ------------------------------
